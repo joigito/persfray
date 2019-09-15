3ac43ea232cca8aab2b4ba3e3bed1ebdd3fc4bbd
Jorge Diaz <jorgediaz@hotmail.com>
2019-09-15 11:53:42 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'personals#index'
   resources :inasistencia
   resources :personals
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
